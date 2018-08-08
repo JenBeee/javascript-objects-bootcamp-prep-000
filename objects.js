dfe3825cf763134b82a175474bfa84aba7a4546c
Jennifer Bishop <jbishop@gmail.com>
2018-08-08 00:08:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,10 @@ function updatePlaylist(listname, artist, song){
   listname[artist] = song
   return listname
 }
+updatePlaylist(playlist, Beatles, 'Love Me Do')
+
 function removeFromPlaylist(listname, artist){
   delete listname.artist
   return listname
 }
+removeFromPlaylist(playlist, Beatles)
