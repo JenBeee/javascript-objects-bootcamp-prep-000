195285c5cb796c177a0c22ea9ac5bd177da0a26c
Jennifer Bishop <jbishop@gmail.com>
2018-08-08 00:24:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,13 @@
 var playlist = {Prince: 'Purple Rain'}
-console.log(playlist)
+
 
 function updatePlaylist(listname, artist, song){
   listname[artist] = song
-  console.log(listname)
+  return listname
 }
-updatePlaylist(playlist, 'Beatles', 'Love Me Do')
+
 
 function removeFromPlaylist(listname, artist){
   delete listname[artist]
-  console.log(listname)
+  return listname
 }
-removeFromPlaylist(playlist, 'Beatles')
